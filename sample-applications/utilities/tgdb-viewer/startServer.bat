e29da99dc4386dfc545ab6e4ddf62b62bfd6361a
SteveNY-Tibco <chuyang@tibco.com>
2020-12-31 11:33:27 -0500

    Remove utilities which is not part of this project

--- sample-applications/utilities/tgdb-viewer/startServer.bat
+++
@@ -1,5 +0,0 @@
-set GRAPH_BUILDER_HOME=%CD%
-
-start "TGDBViewer" java -cp ./bin;./exeJar/tgdb-terminal-0.9.0.jar com.tibco.tge.main.TGDBViewer -s ./server.json
-
-pause
--- sample-applications/utilities/tgdb-viewer/startServer.sh
+++
@@ -1,3 +0,0 @@
-export GRAPH_BUILDER_HOME=$(pwd)
-
-java -cp ./bin:./exeJar/tgdb-terminal-0.9.0.jar com.tibco.tge.main.TGDBViewer -s ./server.json
